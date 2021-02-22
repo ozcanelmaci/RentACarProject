d676be29666483ab5dbb407550f9db49211eb092
Özcan Elmacı <ozcan.elmaci@ogr.deu.edu.tr>
2021-02-23 02:13:59 +0300

    Fluent Validation desteği eklendi

---
+++ Business/ValidationRules/FluentValidation/CarValidator.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class CarValidator : AbstractValidator<Car>
+    {
+        public CarValidator()
+        {
+            RuleFor(p => p.Description).MinimumLength(2);
+            RuleFor(p=>(int)(p.DailyPrice)).GreaterThan(0);
+        }
+    }
+}
---
+++ Core/CrossCuttingConcerns/Validation/ValidationTool.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.CrossCuttingConcerns.Validation
+{
+    public static class ValidationTool
+    {
+        //vereceğimiz parametreyi en genel interface olarak veririz. İnterface referans tutucu olduğu için.
+        public static void Validate(IValidator validator, object entity)
+        {
+            var context = new ValidationContext<object>(entity);
+            var result = validator.Validate(context);
+            if (!result.IsValid)
+            {
+                throw new ValidationException(result.Errors);
+            }
+        }
+    }
+}
